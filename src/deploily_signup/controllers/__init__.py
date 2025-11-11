d853399d489cbc86f1dd7f47a6f106923d622288
benadda fatima <fatima.benadda@transformatek.dz>
2025-11-11 11:55:56 +0100

    add terms and conditions functionality to signup module, including new fields, templates, and controllers

--- src/deploily_signup/controllers/__init__.py
+++ src/deploily_signup/controllers/__init__.py
@@ -2,3 +2,6 @@
 
 from . import controllers
 from . import login
+from . import event_controller
+from . import contactus_controller
+from . import address_controller
--- src/deploily_signup/models/website.py
+++ src/deploily_signup/models/website.py
@@ -14,13 +14,18 @@ class DeploilyWebsite(models.Model):
     terms_conditions_page = fields.Many2one(
         "website.page", string="Terms and Conditions Page"
     )
+    terms_conditions_text=fields.Char(string="Terms and Conditions Text",default="By creating an account, you confirm that you have read and agree to our")
     privacy_policy_page = fields.Many2one("website.page", string="Privacy Policy Page")
     recaptcha_site_key = fields.Char(string="reCAPTCHA Site Key")
     recaptcha_secret_key = fields.Char(string="reCAPTCHA Secret Key")
 
+
     def get_recaptcha_site_key(self):
         self.ensure_one()
         return self.recaptcha_site_key
+    def get_terms_conditions_text(self):
+        self.ensure_one()
+        return self.terms_conditions_text
 
     # def terms_conditions_page(self):
     #     self.ensure_one()
