87f6b4d23d13a62cae64aef259eeb2897200b0ec
benadda fatima <fatima.benadda@transformatek.dz>
2025-12-21 15:55:05 +0100

    add translation support for terms and conditions text field

--- src/deploily_signup/models/website.py
+++ src/deploily_signup/models/website.py
@@ -14,7 +14,7 @@ class DeploilyWebsite(models.Model):
     terms_conditions_page = fields.Many2one(
         "website.page", string="Terms and Conditions Page"
     )
-    terms_conditions_text=fields.Char(string="Terms and Conditions Text",default="By creating an account, you confirm that you have read and agree to our")
+    terms_conditions_text=fields.Char(string="Terms and Conditions Text",default="By creating an account, you confirm that you have read and agree to our", translate=True )
     privacy_policy_page = fields.Many2one("website.page", string="Privacy Policy Page")
     recaptcha_site_key = fields.Char(string="reCAPTCHA Site Key")
     recaptcha_secret_key = fields.Char(string="reCAPTCHA Secret Key")
