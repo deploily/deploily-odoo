340710a4ff6a24effb043670347e4938f5dc7b69
Boushaba Saadia <saadia.boushaba@transformatek.dz>
2025-12-30 11:24:53 +0100

    delete deploily_signup

--- src/deploily_signup/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import controllers
-from . import login
-from . import event_controller
-from . import contactus_controller
-from . import address_controller
-from . import website_info
