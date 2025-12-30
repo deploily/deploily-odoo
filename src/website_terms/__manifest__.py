10f019e967da688b0e64fb42addb40e33e08312b
Boushaba Saadia <saadia.boushaba@transformatek.dz>
2025-12-30 10:28:38 +0100

    add website_terms module

---
+++ src/website_terms/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    "name": "website_terms",
+    "ressource": """
+        Website Terms and Conditions and Privacy Policy Management
+    """,
+    "author": "SARL Transformatek",
+    "website": "https://transformatek.dz",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    "category": "Uncategorized",
+    "version": "0.1",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "auth_signup",
+        # "contacts",
+        "website",
+    ],
+    # always loaded
+    "data": [
+        "views/website_views.xml",
+        "views/signup_login_templates.xml",
+        # "views/menus.xml",
+        # "views/newsletter_template.xml",
+        "data/contactus_templates.xml",
+    ],
+    "license": "Other proprietary",
+    "assets": {
+        "web.assets_frontend": [],
+    },
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ src/website_terms/controllers/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import contactus_controller
+from . import website_info
+from . import login
