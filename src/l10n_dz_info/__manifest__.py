b1793d1ec468ea5cfa308c65f3bc1e43f1546cc9
Ali BENAHMED DAHO <ali.benahmeddaho@transformatek.dz>
2026-08-30 22:57:09 +0100

    Update i10n_dz_info

--- src/l10n_dz_info/__manifest__.py
+++ src/l10n_dz_info/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Algeria - Accounting',
-    'version': '18.0.1.0.0',
-    'category': 'Localization',
-    'description': """
+    "name": "DZ - Info",
+    "version": "18.0.1.0.0",
+    "category": "Localization",
+    "description": """
 This is the module to manage the accounting chart for Algeria in Odoo.
 ========================================================================
 
@@ -11,19 +11,18 @@ This module applies to companies based in Algeria.
 
 **Email:** contact@osis.dz
 """,
-    'author': 'Osis + SARL Transformatek',
-    'website': 'https://deploily.cloud',
-    'depends': ['account', 'sale'],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/dz_wilayas.xml',
-        'data/dz_commune.xml',
-        'views/l10n_dz_info_view.xml',
-        'reports/l10n_dz_info_external_layout.xml',
+    "author": "Osis + SARL Transformatek",
+    "website": "https://deploily.cloud",
+    "depends": ["account", "sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/dz_wilayas.xml",
+        "data/dz_commune.xml",
+        "views/l10n_dz_info_view.xml",
+        "reports/l10n_dz_info_external_layout.xml",
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-        'license': 'LGPL-3',
-
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
